7ac4c0ba4c05c7ffeb704c693a0cf327442b5124
Yanochka <burnwithmenow@gmail.com>
2019-11-13 00:25:00 +0300

    fixed #2

--- src/main/java/FuelService.java
+++ src/main/java/FuelService.java
@@ -1,6 +1,7 @@
 public class FuelService {
     public int calculateDistance(int capacity, int consumption) {
-        int distance = capacity * 100 / consumption;
+        int kilometres = 100;
+        int distance = capacity * kilometres / consumption;
         return distance;
     }
 }
